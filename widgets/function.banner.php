9b3df85c96e51ccd4cae4d210e9da5cdc10ef82b
voronin <voronin@241beb36-3458-467f-8100-dfe2a3c39fdd>
2011-11-29 07:09:16 +0000

    upd: Исправлен баг в баннерах

--- widgets/function.banner.php
+++ widgets/function.banner.php
@@ -10,15 +10,19 @@
 function smarty_function_banner( $params )
 {
     $modelBanner = Model::GetModel('Banner');
-    $banners = $modelBanner->findAll();
     $countBanner = $modelBanner->count();
     if($countBanner==0)
     {
         print "Error! Banner not found!";
         return;
     }
-
-    $banner  = $banners[rand(0,$countBanner-1)];
+    if($countBanner==1){
+        $banner  = $modelBanner->find(array());
+    }
+    else{
+        $banners = $modelBanner->findAll();
+        $banner  = $banners[rand(0,$countBanner-1)];
+    }
     $banner['count_show']=$banner['count_show']+1;
     $modelBanner->save( $banner );
 
