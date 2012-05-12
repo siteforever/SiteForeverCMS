4b0109aaf0d19446fe5d57ab926d8a28cd14411e
keltanas <keltanas@gmail.com>
2012-05-12 20:11:44 +0400

    install upgrade

--- install.php
+++ install.php
@@ -5,63 +5,89 @@
  * @link http://ermin.ru
  * @link http://siteforever.ru
  */
+header("content-type: text/html; charset=cp866");
 
 
-$location = "X:\\home\\";
+error_reporting( 0 );
+define('DS', DIRECTORY_SEPARATOR);
+$location = trim( realpath( __DIR__.DS.'..'.DS.'..' ), DS ) . DS;
 
-$dest = isset($_POST['dest']) ? $_POST['dest'] : false;
+$dest = isset($_POST['dest']) && $_POST['dest'] ? $_POST['dest'] : "";
+$name = isset($_POST['name']) && $_POST['name'] ? $_POST['name'] : "";
 
 
-if ( ! $dest ) {
-    ?>
-    <html>
-    <body>
-    <form action="/install.php" method="post">
-        Destenation path <?=$location?> <input name="dest" value="gal/test" />
-        <input type="submit" value="Create" />
-    </form>
-    </body>
-    </html>
-    <?php
+
+?>
+<html>
+<body>
+<form action="/install.php" method="post">
+    <p>Name project <input name="name" value="<?=$name?>" /></p>
+    <p>Destenation path <?=$location?><input name="dest" value="<?=$dest?>" /></p>
+    <input type="submit" value="Create" />
+</form>
+    <pre><?php
+//    print_r( $_SERVER );
+    ?></pre>
+</body>
+</html>
+<?php
+if ( ! $dest || ! $name ) {
     die();
 }
 
-header("content-type: text/plain; charset=cp866");
-
 $location .= $dest;
 
-$src = dirname(__FILE__);
+$src = __DIR__;
 
-print 'Source: '.$src."\r\n";
+print 'Source: '.$src."<br>\n";
 
-$location = str_replace(array('\\','/'), DIRECTORY_SEPARATOR, $location );
+$location = str_replace(array('\\','/'), DS, $location );
 
-print 'Destenation path: '.$location."\r\n";
+print 'Destenation path: '.$location."<br>\n";
 
-system("mkdir $location");
+//    if ( file_exists( $location ) ) {
+//        unlink( $location );
+//    }
+@mkdir( $location, 0755, true );
+
+//system("mkdir $location");
 
 
-system("mklink /D {$location}\\class {$src}\\class");
-system("mklink /D {$location}\\misc {$src}\\misc");
-system("mklink /D {$location}\\widgets {$src}\\widgets");
-system("mklink /D {$location}\\images {$src}\\images");
-system("mklink /H {$location}\\.htaccess {$src}\\.htaccess");
-system("mklink /H {$location}\\bootstrap.php {$src}\\bootstrap.php");
-system("mklink /H {$location}\\functions.php {$src}\\functions.php");
-system("mklink /H {$location}\\config.php {$src}\\config.php");
-system("mklink /H {$location}\\modules.php {$src}\\modules.php");
+symlink( $src.DS.'misc', $location.DS.'misc' );
+//    system("mklink /D {$location}\\misc {$src}\\misc");
+print "<br>\n";
+//    system("mklink /D {$location}\\images {$src}\\images");
+symlink( $src.DS.'images', $location.DS.'images' );
+print "<br>\n";
 
-system("copy {$src}\\index.php {$location}\\index.php /Y");
-system("copy {$src}\\modules.php {$location}\\modules.php /Y");
+copy( $src.DS.'modules.php', $location.DS.'modules.php' );
+copy( $src.DS.'index.php', $location.DS.'index.php' );
 
-system("mkdir {$location}\\files");
-system("mkdir {$location}\\themes");
-system("xcopy themes {$location}\\themes /S/E/Y");
+copy( $src.DS.'.htaccess', $location.DS.'.htaccess' );
+
+mkdir($location.DS.'files', 0777, true);
+mkdir($location.DS.'themes'.DS.$name, 0777, true);
+mkdir($location.DS.'themes'.DS.$name.DS.'css', 0777, true);
+copy( $src.DS.'themes'.DS.'basic'.DS.'css'.DS.'style.css', $location.DS.'themes'.DS.$name.DS.'css'.DS.'style.css' );
+mkdir($location.DS.'themes'.DS.$name.DS.'js', 0777, true);
+copy( $src.DS.'themes'.DS.'basic'.DS.'js'.DS.'script.js', $location.DS.'themes'.DS.$name.DS.'js'.DS.'script.js' );
+mkdir($location.DS.'themes'.DS.$name.DS.'images', 0777, true);
+mkdir($location.DS.'themes'.DS.$name.DS.'templates', 0777, true);
+copy( $src.DS.'themes'.DS.'basic'.DS.'templates'.DS.'index.tpl', $location.DS.'themes'.DS.$name.DS.'templates'.DS.'index.tpl' );
+copy( $src.DS.'themes'.DS.'basic'.DS.'templates'.DS.'inner.tpl', $location.DS.'themes'.DS.$name.DS.'templates'.DS.'inner.tpl' );
+//    system("mkdir {$location}\\files");
+//    system("mkdir {$location}\\themes");
+//    system("xcopy themes {$location}\\themes /S/E/Y");
 //system("mkdir {$location}\\protected");
 //system("mkdir {$location}\\protected\\_runtime");
-system("mkdir {$location}\\protected\\_runtime\\_cache");
-system("mkdir {$location}\\protected\\_runtime\\_templates_c");
-system("mkdir {$location}\\protected\\config");
-system("mklink /D {$location}\\protected\\lang {$src}\\protected\\lang");
 
-system("copy {$src}\\protected\\config\\main.php {$location}\\protected\\config\\main.php /Y");
+//    mkdir("{$location}\\protected\\_runtime\\_cache", 0777, true);
+mkdir($location.DS."protected".DS."_runtime".DS."_templates_c", 0777, true);
+mkdir($location.DS."protected".DS."config", 0777, true);
+
+//    system("mkdir {$location}\\protected\\_runtime\\_cache");
+//    system("mkdir {$location}\\protected\\_runtime\\_templates_c");
+//    system("mkdir {$location}\\protected\\config");
+//    system("mklink /D {$location}\\protected\\lang {$src}\\protected\\lang");
+
+copy( $src.DS.'protected'.DS.'config'.DS.'main.php', $location.DS.'protected'.DS.'config'.DS.$name.'.php' );
