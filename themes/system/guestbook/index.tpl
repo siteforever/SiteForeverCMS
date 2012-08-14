9a5349cb2cbf3869f715fc93b9cbc0294e72aa69
Nikolay Ermin <keltanas@gmail.com>
2012-08-15 02:30:18 +0400

    Add less support

--- themes/system/guestbook/index.tpl
+++ themes/system/guestbook/index.tpl
@@ -1,9 +1,12 @@
 {foreach from=$messages item="msg"}
-<div class="sfcms_guestbook_message">
-    <div class="sfcms_guestbook_title"><a href="mailto:{$msg->email}">{$msg->name}</a> <small>{$msg->date|date_format:"%x"}</small></div class=&quot;sfcms_guestbook_message&quot;>
-    <div class="sfcms_guestbook_body">{$msg->message|strip_tags|nl2br}</div>
+<div class="guestbook_message">
+    <div class="guestbook_title">
+        <a href="mailto:{$msg->email}">{$msg->name}</a>
+        <small>{$msg->date|date_format:"%x"}</small>
+    </div>
+    <div class="guestbook_body">{$msg->message|strip_tags|nl2br}</div>
     {if $msg->answer}
-        <div class="sfcms_guestbook_answer">
+        <div class="guestbook_answer">
             <strong>{t cat="guestbook"}Answer{/t}:</strong>
             {$msg->answer|strip_tags|nl2br}</div>
     {/if}
--- widgets/function.head.php
+++ widgets/function.head.php
@@ -34,11 +34,21 @@ function smarty_function_head( $params )
         $head[] = "<link rel=\"icon\" type=\"image/ico\" href=\"http://{$_SERVER['HTTP_HOST']}/favicon.ico\" />";
     }
 
+    $useLess = false;
     foreach( $app->getStyle() as $style ) {
-        $head[] = "<style type=\"text/css\">@import url(\"".$style."\");</style>";
+        if ( preg_match('/.*\.css$/', $style) ) {
+            $head[ ] = "<link type=\"text/css\" rel=\"stylesheet\" href=\"$style\">";
+        } elseif ( preg_match('/.*\.less$/', $style) ) {
+            $head[ ] = "<link type=\"text/css\" rel=\"stylesheet/less\" href=\"$style\">";
+            $useLess = true;
+        }
     }
     foreach( $app->getScript() as $script ) {
         $head[] = "<script type=\"text/javascript\" src=\"".$script."\"></script>";
+    }
+
+    if ( $useLess ) {
+        $head[] = '<script type="text/javascript" src="/misc/less-1.3.0.min.js"></script>';
     }
 
     $head[] = "<meta name=\"generator\" content=\"SiteForever CMS\" />";
