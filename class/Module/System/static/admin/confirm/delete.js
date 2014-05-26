3c1235d119879454ebb62b5cbcfadce8eb92f579
keltanas <keltanas@gmail.com>
2014-05-26 18:52:21 +0400

    fix deleting images from gallery

--- class/Module/System/static/admin/confirm/delete.js
+++ class/Module/System/static/admin/confirm/delete.js
@@ -10,8 +10,26 @@ define("system/admin/confirm/delete", [
     return function(e) {
         e.preventDefault();
         var node = e.currentTarget,
-            $dialogDeleteConfirm = $('#dialog-delete-confirm');
-        console.log(e, node);
+            $dialogDeleteConfirm = $('#dialog-delete-confirm'),
+            buttons = function($dialogDeleteConfirm, node) {
+                return [
+                    {text:i18n('Ok'),'click': function(){
+                        var $dialog = $(this);
+                        $dialog.html('<div class="center"><img src="/static/system/images/progress-bar.gif"></div>');
+                        $.post($(node).attr('href'), function (result) {
+                            if (!result.error) {
+                                $('li[data-id="' + result.id + '"]').remove();
+                            }
+                            $dialog.dialog('close');
+                            $.growlUI(i18n('Delete successfully'));
+                        }, "json");
+                    }},
+                    {text:i18n('Cancel'), 'click': function(){
+                        $(this).dialog('close');
+                    }}
+                ];
+            };
+
         if (!$dialogDeleteConfirm.length) {
             $dialogDeleteConfirm = $('<div id="dialog-delete-confirm"></div>').appendTo('body');
             $dialogDeleteConfirm
@@ -20,26 +38,13 @@ define("system/admin/confirm/delete", [
                     autoOpen: false,
                     resizable: false,
                     height:140,
-                    modal: true,
-                    buttons: [
-                        {text:i18n('Ok'),'click': function(){
-                            var $dialog = $(this);
-                            $dialog.html('<div class="center"><img src="/static/system/images/progress-bar.gif"></div>');
-                            $.post($(node).attr('href'), function (result) {
-                                if (!result.error) {
-                                    $('li[data-id="' + result.id + '"]').remove();
-                                }
-                                $dialog.dialog('close');
-                                $.growlUI(i18n('Delete successfully'));
-                            }, "json");
-                        }},
-                        {text:i18n('Cancel'), 'click': function(){
-                            $(this).dialog('close');
-                        }}
-                    ]
+                    modal: true
                 });
         }
 
-        $dialogDeleteConfirm.html(i18n('The data will be lost. Do you really want to delete?')).dialog('open');
+        $dialogDeleteConfirm
+            .dialog('option', 'buttons', buttons($dialogDeleteConfirm, node))
+            .html(i18n('The data will be lost. Do you really want to delete?'))
+            .dialog('open');
     };
 });
---
+++ info.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * This file is part of the @package@.
+ *
+ * @author: Nikolay Ermin <keltanas@gmail.com>
+ * @version: @version@
+ */
+phpinfo();
